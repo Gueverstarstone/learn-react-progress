054ec173a22dc7c3fa3f63e4e09303306df4a3f6
gueverstarstone <josephmungai028@gmail.com>
2025-11-05 23:07:14 +0300

    Added some knowledge in react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import User from "./components/User";
+
+function App() {
+  return (
+    <>
+      <User name="Mungai!" message="Welcome home"/>
+  
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/User.jsx
@@ -0,0 +1,9 @@
+ function User(props) {
+  return (
+    <div>
+      <h1>{props.message} {props.name}</h1>
+    </div>
+  )
+}
+
+export default User
